e93d1db322161e153878fe0e92a31cb1bc27090e
laupgm <111069320+laupgm@users.noreply.github.com>
2025-05-13 13:24:38 +0100

    backend

--- mypose_server.py
+++ mypose_server.py
@@ -1,86 +1,49 @@
-from ultralytics import YOLO
 import cv2
 from PIL import Image
 import base64
 
-
 import os
 
 from werkzeug.utils import secure_filename
-
 from flask import Flask, render_template, request, Response, url_for, render_template_string, send_from_directory
 
+from BlazeposeOpenvino import BlazeposeOpenvino, POSE_DETECTION_MODEL, LANDMARK_MODEL_FULL
 
-model = YOLO("yolo11s-pose.pt") # very good model
-
-#VIDEO_SOURCE = "../videos/mixkit-cargo-ship-arriving-to-container-terminal-30979-hd-ready.mp4"
-#VIDEO_SOURCE = "./video/wheelchair-exercises.mp4"
-#VIDEO_SOURCE = "./video/sports-runner.mp4"
+OUTPUT_IMG = "output.jpg"
 
 app = Flask(__name__, static_folder='static')
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#@app.route('/')
-#def home():
-#    return render_template('index.html')
-
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# HTML templates as raw strings
-#upload_page = '''
-#<!doctype html>
-#<html lang="en">
-#<head>
-#    <meta charset="UTF-8">
-#    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-#    <title>Upload an Image</title>
-#    <link rel="stylesheet" href="{{ url_for('static', filename='styles.css') }}">
-#</head>
-#<body>
-#    <h1>Upload a picture</h1>
-#    <form method=post enctype=multipart/form-data>
-#    <input type=file name=file>
-#    <input type=submit value=Upload>
-#    </form>
-#</body>
-#</html>
-#'''
-
-#display_page = '''
-#<!doctype html>
-#<html lang="en">
-#<title>Image Uploaded</title>
-#<h1>Here is your image:</h1>
-#<img src="data:image/jpeg;base64,{{ filename }}" alt="Image"/>
-
-#<!-- <img src="{{ url_for('uploaded_file', filename=filename) }}"> -->
-
-#<!-- {{ url_for('uploaded_file', filename=filename) }} -->
-
-#<br><br>
-#<a href="{{ url_for('upload') }}">Upload another</a>
-#'''
 
 ## IMAGE
-
 def process_image(img):
 
     print("-- opening ", img)
-    frame = Image.open(img)
+    #frame = Image.open(img)
 
-    results = model(frame)
-    annotated_frame = results[0].plot()
-    #cv2.imshow("YOLO Inference", annotated_frame)
-    imgencode = cv2.imencode('.jpg', annotated_frame)[1]
-    return base64.b64encode(imgencode).decode('utf-8')
+    ht = BlazeposeOpenvino(input_src=img,
+                pd_xml=POSE_DETECTION_MODEL,
+                pd_device="CPU",
+                pd_score_thresh=0.5, pd_nms_thresh=0.3,
+                lm_xml=LANDMARK_MODEL_FULL,
+                lm_device="CPU",
+                lm_score_threshold=0.5,
+                use_gesture=False,
+                smoothing=True,
+                filter_window_size=5,
+                filter_velocity_scale=10,
+                show_3d=False,
+                crop=False,
+                multi_detection=False,
+                force_detection=False,
+                output=OUTPUT_IMG)
+
+    return ht.run()
 
 ##
-
-@app.route('/vid')
-def vid():
-     return Response(get_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def upload():
